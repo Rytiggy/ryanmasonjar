60723d388d1b9750f3383ba1808c2a40a910e776
Ryan Mason <ryanmasonjar@gmail.com>
2019-06-26 09:07:02 -0400

    fix ui

---
+++ .quasar/import-quasar.js
@@ -0,0 +1,21 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.conf.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+import iconSet from 'quasar/icon-set/fontawesome-v5'
+
+
+import Vue from 'vue'
+
+import {Quasar,QLayout,QHeader,QDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QItem,QItemSection,QItemLabel,QBadge,QCard,QCardSection,QCardActions,QCarousel,QCarouselControl,QCarouselSlide,QSlideTransition,QSeparator,QImg,QChip,QAvatar,Ripple,Notify} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},iconSet: iconSet,components: {QLayout,QHeader,QDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QItem,QItemSection,QItemLabel,QBadge,QCard,QCardSection,QCardActions,QCarousel,QCarouselControl,QCarouselSlide,QSlideTransition,QSeparator,QImg,QChip,QAvatar},directives: {Ripple},plugins: {Notify} })
